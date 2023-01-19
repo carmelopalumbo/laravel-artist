c9074356f07d2fca4a07fadc920059818754b872
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-19 16:47:31 +0100

    [add] data to seeder

--- database/seeders/ArtworksTableSeeder.php
+++ database/seeders/ArtworksTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Artwork;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,15 @@ class ArtworksTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $artwork_data = config('db.artworks');
+
+        foreach ($artwork_data as $artwork) {
+            $new_artwork = new Artwork();
+            $new_artwork->artist_id = $artwork['artist_id'];
+            $new_artwork->museum_id = $artwork['museum_id'];
+            $new_artwork->name = $artwork['name'];
+            $new_artwork->year = $artwork['year'];
+            $new_artwork->save();
+        }
     }
 }
